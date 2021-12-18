4ca3c5af4a9cc1f8e915261498e830f6b5f5253a
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-12-18 13:39:59 +0100

    Add business contacts class

--- business_cards.py
+++ business_cards.py
@@ -7,6 +7,25 @@ class BaseContact:
         self.last_name = last_name
         self.phone = phone
         self.email = email
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.phone} {self.email}"
+    
+    def contact(self):
+        return f".. Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.last_name}"
+
+class BusinessContact(BaseContact):
+    def __init__(self, position, company, business_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.position = position
+        self.company = company
+        self.business_phone = business_phone
+
+    def __str__(self):
+        return super().__str__() + f" {self.position} {self.company} {self.business_phone}"
+    
+    def contact(self):
+        return f".. Wybieram numer {self.business_phone} i dzwonię do {self.first_name} {self.last_name}"
 
 card1 = BaseContact(
     first_name = fake.first_name(),
@@ -15,4 +34,15 @@ card1 = BaseContact(
     email = fake.email()
 )
 
-print(card1.phone)
+card2 = BusinessContact(
+    first_name = fake.first_name(),
+    last_name = fake.last_name(),
+    phone = fake.phone_number(),
+    email = fake.email(),
+    position = fake.job(),
+    company = fake.company(),
+    business_phone = fake.phone_number()
+)
+
+print(card2)
+print(card2.contact())
