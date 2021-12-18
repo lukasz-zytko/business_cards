fed8ffed641f9543a8d89321e3af9d5b1ff15419
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-12-18 13:07:37 +0100

    Add base contact

--- business_cards.py
+++ business_cards.py
@@ -1,3 +1,18 @@
 from faker import Faker
-fake = Faker("pl-PL")
+fake = Faker("pl_PL")
 
+class BaseContact:
+    def __init__(self, first_name, last_name, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+
+card1 = BaseContact(
+    first_name = fake.first_name(),
+    last_name = fake.last_name(),
+    phone = fake.phone_number(),
+    email = fake.email()
+)
+
+print(card1.phone)
